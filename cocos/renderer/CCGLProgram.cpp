57e23127ea18e5367a984655a1c7391b3c3ce49e
WuJiayiSH <wujiayish@msn.com>
2021-02-18 15:40:06 +0800

    Fixed "uniform CC_PMatrix is not linkable between attached shaders" on firefox/electron

--- cocos/renderer/CCGLProgram.cpp
+++ cocos/renderer/CCGLProgram.cpp
@@ -488,7 +488,7 @@ bool GLProgram::compileShader(GLuint * shader, GLenum type, const GLchar* source
         headersDef = (type == GL_VERTEX_SHADER ? "precision mediump float;\n precision mediump int;\n" : "precision mediump float;\n precision mediump int;\n");
 // Bugfix to make shader variables types constant to be understood by the current Android Virtual Devices or Emulators. This will also eliminate the 0x501 and 0x502 OpenGL Errors during emulation.
 // Changed shader data types mediump to highp to remove possible sprite joggling on some Android phones.
-#elif CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+#elif CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_EMSCRIPTEN
         headersDef = (type == GL_VERTEX_SHADER ?
             "#version 100\n precision highp float;\n precision highp int;\n" :
             "#version 100\n precision highp float;\n precision highp int;\n");
